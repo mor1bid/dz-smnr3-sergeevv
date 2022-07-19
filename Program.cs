f0096b80bdc429c72100cd7e17adb24d854d99f7
De <spark49942@gmail.com>
2022-07-19 21:52:17 +0300

    Zadacha23runreversef

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ while (count < ind)
     while (countnum < num) 
         {
             countnum += 1;
-            string numc = ($"{countnum*countnum*countnum}");
+            string numc = ($"{countnum^3}");
             Console.WriteLine(countnum + "^3 = " + numc);
         }
 count++;
