b5ce0456e0bd03e56343519b499924acdf144869
De <spark49942@gmail.com>
2022-07-19 20:25:52 +0300

    Zadacha21

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿Console.WriteLine("Введите число:");
-int num = Convert.ToInt32(1000-9999);
-if (num % 2 == 0) {
-    num %= 10;
-    Console.WriteLine("Число чётное. Остаток: " + num); }
-    else 
-    {
-    num %= 10;
-    Console.WriteLine("Число нечётное Остаток: " + num);
-    }
+﻿// void Zadacha19 () {
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine.Next());
+// if (num >= 10000 && num <= 9999) 
+// num %= 10;
+
+// }
+// Zadacha19();
+
+void Zadacha21 () {
+int dist;
+Console.WriteLine("Введите координаты x, y, z точки A: ");
+int xA = Convert.ToInt32(Console.ReadLine()),
+yA = Convert.ToInt32(Console.ReadLine()),
+zA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты x, y, z точки B: ");
+int xB = Convert.ToInt32(Console.ReadLine()),
+yB = Convert.ToInt32(Console.ReadLine()),
+zB = Convert.ToInt32(Console.ReadLine());
+
+dist = (xA-xB)^2+(yA-yB)^2+(zA-zB)^2;
+Console.WriteLine(dist);
+}
+Zadacha21();
