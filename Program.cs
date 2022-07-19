e7ab452e04552d6f1838e69e4b95c47d3bff1d24
De <spark49942@gmail.com>
2022-07-19 20:40:12 +0300

    Zadacha21cycle

--- Program.cs
+++ Program.cs
@@ -8,17 +8,21 @@
 // Zadacha19();
 
 void Zadacha21 () {
-double dist;
-Console.WriteLine("Введите координаты x, y, z точки A: ");
-int xA = Convert.ToInt32(Console.ReadLine()),
-yA = Convert.ToInt32(Console.ReadLine()),
-zA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты x, y, z точки B: ");
-int xB = Convert.ToInt32(Console.ReadLine()),
-yB = Convert.ToInt32(Console.ReadLine()),
-zB = Convert.ToInt32(Console.ReadLine());
-
-dist = Math.Sqrt(Math.Pow(xB-xA, 2)+ Math.Pow(yB-yA, 2)+ Math.Pow(zB-zA, 2));
-Console.WriteLine(dist);
+int ind = 999, count = 0;
+while(ind > count) 
+{
+    double dist;
+    Console.WriteLine("Введите координаты x, y, z точки A: ");
+    int xA = Convert.ToInt32(Console.ReadLine()),
+    yA = Convert.ToInt32(Console.ReadLine()),
+    zA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координаты x, y, z точки B: ");
+    int xB = Convert.ToInt32(Console.ReadLine()),
+    yB = Convert.ToInt32(Console.ReadLine()),
+    zB = Convert.ToInt32(Console.ReadLine());
+    dist = Math.Sqrt(Math.Pow(xB-xA, 2)+ Math.Pow(yB-yA, 2)+ Math.Pow(zB-zA, 2));
+    Console.WriteLine(dist);
+}
+count++;
 }
 Zadacha21();
