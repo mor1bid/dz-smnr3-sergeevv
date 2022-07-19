0c0f897b9af1b7fd4fdf111e1ce5f92c8bee473d
De <spark49942@gmail.com>
2022-07-19 19:13:42 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(1000-9999);
+if (num % 2 == 0) {
+    num %= 10;
+    Console.WriteLine("Число чётное. Остаток: " + num); }
+    else 
+    {
+    num %= 10;
+    Console.WriteLine("Число нечётное Остаток: " + num);
+    }
