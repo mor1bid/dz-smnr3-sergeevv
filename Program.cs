5b2df857c0b21134df50ce646f00eb2cb68f7b0f
De <spark49942@gmail.com>
2022-07-20 14:31:39 +0300

    19-32

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
          {
          int reverse1=0, reverse2=0;
          //int number = new Random().Next(10000, 99999);
-         int num = 12831;
+         int num = 13821;
          Console.WriteLine(num);
          int amun = num;
          while (amun > 0)
